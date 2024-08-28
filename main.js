e4bf0f18e206a0c29787f924d19ad25215e3bac5
Dandev-git <danielperezcelemin@outlook.com>
2024-08-28 17:01:25 -0400

    checged saving system

--- main.js
+++ main.js
@@ -3,16 +3,10 @@ const form = document.querySelector('form');
 const input = document.getElementById('todo');
 const clearBtn = document.getElementById('clear');
 
-const oldTodos = JSON.parse(localStorage.getItem('todos'));
-let todoData = oldTodos || [];
-
-todoData.forEach(todo => {
-    createTodo(todo);
-});
+todos.innerHTML = JSON.parse(localStorage.getItem('todos'));
 
 form.onsubmit = (Event) => {
     Event.preventDefault();
-    todoData.push(input.value);
     createTodo(input.value);
     form.reset();
 }
@@ -22,26 +16,23 @@ function createTodo(text) {
     const todo = document.createElement('li');
     todo.innerHTML = text;
     const span = document.createElement('span');
-    span.innerHTML = '\u00d7'
+    span.innerHTML = '\u00d7';
     todos.appendChild(todo);
     todo.appendChild(span);
-    localStorage.setItem('todos', JSON.stringify(todoData));
+    localStorage.setItem('todos', JSON.stringify(todos.innerHTML));
     todo.onclick = () => {
         if (todo.className !== 'checked') {
             todo.className = 'checked';
         } else {
             todo.className = '';
         }
+        localStorage.setItem('todos', JSON.stringify(todos.innerHTML));
     }
 
     span.onclick = () => {
-        todoData = todoData.filter(item => {
-            return item !== todo.innerHTML.replace('<span>×</span>', '');
-        })
-
-        localStorage.setItem('todos', JSON.stringify(todoData));
-
         todo.remove();
         span.remove();
+
+        localStorage.setItem('todos', JSON.stringify(todos.innerHTML));
     }
 }
