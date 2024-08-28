a6c96a7061394c6bf4e1ebf2b9b9ae0a3cd62c8f
Dandev-git <danielperezcelemin@outlook.com>
2024-08-28 17:17:31 -0400

    added saving the state of todos

--- main.js
+++ main.js
@@ -3,36 +3,48 @@ const form = document.querySelector('form');
 const input = document.getElementById('todo');
 const clearBtn = document.getElementById('clear');
 
-todos.innerHTML = JSON.parse(localStorage.getItem('todos'));
+const oldTodos = JSON.parse(localStorage.getItem('todos'));
+let todoData = oldTodos || [];
+
+todoData.forEach(todo => {
+    createTodo(todo.text, todo.className);
+});
 
 form.onsubmit = (Event) => {
     Event.preventDefault();
-    createTodo(input.value);
+    const newTodo = { text: input.value, className: '' };
+    todoData.push(newTodo);
+    createTodo(newTodo.text, newTodo.className);
     form.reset();
 }
 
-function createTodo(text) {
+function createTodo(text, className) {
 
     const todo = document.createElement('li');
     todo.innerHTML = text;
+    todo.className = className || '';
+
     const span = document.createElement('span');
-    span.innerHTML = '\u00d7';
+    span.innerHTML = '\u00d7'
     todos.appendChild(todo);
     todo.appendChild(span);
-    localStorage.setItem('todos', JSON.stringify(todos.innerHTML));
+    localStorage.setItem('todos', JSON.stringify(todoData));
+
     todo.onclick = () => {
-        if (todo.className !== 'checked') {
-            todo.className = 'checked';
-        } else {
-            todo.className = '';
-        }
-        localStorage.setItem('todos', JSON.stringify(todos.innerHTML));
+        todo.classList.toggle('checked');
+        const todoIndex = Array.from(todos.children).indexOf(todo);
+        todoData[todoIndex].className = todo.className;
+        localStorage.setItem('todos', JSON.stringify(todoData));
     }
 
-    span.onclick = () => {
+    span.onclick = (e) => {
+        e.stopPropagation();
+        const todoIndex = Array.from(todos.children).indexOf(todo);
+        todoData.splice(todoIndex, 1);
+
+        localStorage.setItem('todos', JSON.stringify(todoData));
+
         todo.remove();
         span.remove();
-
-        localStorage.setItem('todos', JSON.stringify(todos.innerHTML));
     }
 }
